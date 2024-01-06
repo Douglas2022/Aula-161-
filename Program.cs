c7db34977f6bc7e38623e22ef2645cb92c684352
Douglas <paulodnunes2016@gmail.com>
2024-01-06 20:40:56 -0300

    A prendendo a fazer xadrez

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using tabuleiro;
+namespace Xadre___console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Tabuleiro tab = new Tabuleiro(8,8);
+
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,17 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        private Peca[,] peca;
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+            
+            peca = new Peca[linha,coluna];
+        }
+    }
+}
